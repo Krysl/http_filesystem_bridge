31e4668e82c182ee9970539ef7de93d1e4249160
Krysl <krysl@qq.com>
2025-05-17 15:52:31 +0800

    feat: 🔖 show versions in cmd line help,  set version to 0.2.0
    Signed-off-by: Krysl <krysl@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,20 @@ use url::Url;
 use widestring::{U16CString, U16String};
 
 fn command() -> Command {
-    Command::new("dokan-rust memfs example")
+    Command::new("Http FileSystem bridge")
+        .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
+        .help_template(
+"\
+{name} {version}
+{about-with-newline}
+USAGE:
+    {usage}
+
+OPTIONS:
+{options}
+"
+        )
         .arg(
             Arg::new("mount_point")
                 .short('m')
